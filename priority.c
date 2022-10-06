95b04422aee835f18178cc4100c5999132b46e6a
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-10-07 00:27:31 +0530

    fix: priority

--- priority.c
+++ priority.c
@@ -2,78 +2,82 @@
 #include<string.h>
 
 void getData();
+void findPriority();
 void calculate();
 void display();
-void priority();
-int bt[20], wt[20], et[20], pt[20], twt = 0, temp1, temp2, n, ttt = 0, tt[20];
-char pName[20][20], temp[20];
-float awt, att;
+
+int burstTime[20], waitingTime[20], turnAroundTime[20], exitTime[20], priority[20];
+int n, temp, totalWaitingTime, totalTurnAroundTime;
+char pName[20][20], temp1[20];
+float averageWaitingTime, averageTurnAroundTime;
+
 void main() {
     getData();
-    priority();
+    findPriority();
     calculate();
     display();
 }
+
 void getData() {
-    printf("Enter the no of process : ");
-    scanf("%d", & n);
-    for (int i = 1; i <= n; i++) {
-        printf("\nEnter the process name: ");
-        scanf("%s", & pName[i]);
+    printf("Enter the No of Process: ");
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++ ) {
+        printf("\nEnter the Process Name: ");
+        scanf("%s", &pName[i]);
         printf("Enter the Priority: ");
-        scanf("%d", & pt[i]);
-        printf("Enter the burst time: ");
-        scanf("%d", & bt[i]);
+        scanf("%d", &priority[i]);
+        printf("Enter the BurstTime: ");
+        scanf("%d", &burstTime[i]);
     }
 }
-void priority() {
-    for (int i = 1; i <= n; i++) {
-        for (int j = i + 1; j <= n; j++) {
-            if (pt[i] > pt[j]) {
-                temp1 = pt[i];
-                pt[i] = pt[j];
-                pt[j] = temp1;
-                temp2 = bt[i];
-                bt[i] = bt[j];
-                bt[j] = temp2;
-                strcpy(temp, pName[i]);
+
+void findPriority() {
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1 ; j <= n; j++) {
+            if(priority[i] > priority[j]) {
+                temp = priority[i];
+                priority[i] = priority[j];
+                priority[j] = temp;
+                temp = burstTime[i];
+                burstTime[i] = burstTime[j];
+                burstTime[j] = temp;
+                strcpy(temp1, pName[i]);
                 strcpy(pName[i], pName[j]);
-                strcpy(pName[j], temp);
+                strcpy(pName[j], temp1);
             }
         }
     }
 }
+
 void calculate() {
-    int i;
-    wt[1] = 0;
-    tt[1] = 0;
-    tt[1] = bt[1];
-    et[1] = bt[1];
-    et[1] = bt[1];
-    ttt = tt[1];
-    twt = wt[1];
-    for (i = 2; i <= n; i++) {
-        wt[i] = et[i - 1];
-        tt[i] = wt[i] + bt[i];
-        et[i] = et[i - 1] + bt[i];
-        twt = twt + wt[i];
-        ttt = ttt + tt[i];
+    waitingTime[0] = 0;
+    turnAroundTime[0] = burstTime[0] + waitingTime[0];
+    exitTime[0] = burstTime[0];
+
+    totalTurnAroundTime = turnAroundTime[0];
+    totalWaitingTime = waitingTime[0];
+
+    for(int i = 1; i <= n; i++) {
+        waitingTime[i] = exitTime[i-1];
+        turnAroundTime[i] = waitingTime[i] + burstTime[i];
+        exitTime[i] = exitTime[i-1] + burstTime[i];
+
+        totalTurnAroundTime = totalTurnAroundTime + turnAroundTime[i];
+        totalWaitingTime = totalWaitingTime + waitingTime[i];
     }
-    awt = (float) twt / n;
-    att = (float) ttt / n;
-    printf("\nAverage Turn around time=%3.2f ms", att);
-    printf("\nAverage Waiting Time=%3.2f ms", awt);
+    averageTurnAroundTime = (float) totalTurnAroundTime / n;
+    averageWaitingTime = (float) totalWaitingTime / n;
 }
+
 void display() {
-    int i;
-    printf("\n\n\t\tGANTT CHART\n");
-    printf("\n \n");
-    for (i = 1; i <= n; i++) printf("|\t%s\t", pName[i]);
-    printf("|\t\n");
-    printf("\n \n");
-    printf("\n");
-    for (i = 0; i <= n; i++) {
-        printf("%d\t\t", et[i]);
+    printf("\nGantt Chart");
+    printf("\n\n");
+    for(int i = 0; i <= n; i++)
+        printf("|\t%s\t", pName[i]);
+    printf("|\t\n\n\n");
+    for (int i = 0; i <= n; i++) {
+        printf("%d\t\t", exitTime[i]);
     }
-    printf("\n \n");
+    printf("\nAverage TAT: %f", averageTurnAroundTime);
+    printf("\nAverage WT: %f", averageWaitingTime);
 }
