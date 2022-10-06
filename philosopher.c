f097f6ea42578cd243f6f171af84ef3af0d99019
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-10-07 01:51:19 +0530

    fix: philosopher

--- philosopher.c
+++ philosopher.c
@@ -7,42 +7,43 @@
 #define N 5
 #define right ph
 #define left (ph % N) + 1
-sem_t chopstick[5];
+sem_t chopsticks[5];
 pthread_t T[N];
-void * philo(void * );
+
+void * philo(void *);
 void think(int);
 void eat(int);
+
 void main() {
-    int
-    var [N];
-    for (int i = 1; i <= N; i++) {
-        sem_init( & chopstick[i], 0, 1);
+    int var [N];
+    for(int i = 1; i <= N; i++) {
+        sem_init(&chopsticks[i], 0, 1);
     }
-    for (int i = 1; i <= N; i++) {
-        var [i] = i;
-        pthread_create( & T[i], NULL, (void * ) philo, (void * ) &
-            var [i]);
+    for(int i = 1; i <= N; i++) {
+        var[i] = i;
+        pthread_create(&T[i], NULL, (void *) philo, (void *) &var[i]);
     }
-    for (int i = 1; i <= N; i++) {
+    for(int i = 1; i <= N; i++) {
         pthread_join(T[i], NULL);
     }
 }
+
 void * philo(void * n) {
     while (1) {
-        int ph = * (int * ) n;
+        int ph = * (int *) n;
         think(ph);
-        printf("\nPhilosopher %d is HUNGRY & wants to Eat", ph);
-        printf("\nPhilosopher %d tries to pick up left chopstick", ph);
-        sem_wait( & chopstick[left]);
-        printf("\nPhilosopher %d has successfully picked up the left chopstick -> %d", ph, left);
-        printf("\nPhilosopher %d tries to pick the right chopstick", ph);
-        sem_wait( & chopstick[right]);
+        printf("\nPhilosopher %d is Hungry & wants to eat", ph);
+        printf("\nPhilosopher %d tries to pick left chopsticks", ph);
+        sem_wait(&chopsticks[left]);
+        printf("\nPhilosopher %d has successfully picked left chopsticks", ph);
+        printf("\nPhilosopher %d tries to pick right chopsticks", ph);
+        sem_wait(&chopsticks[right]);
         printf("\nPhilosopher %d has successfully picked up the right chopstick -> %d", ph, right);
         eat(ph);
         printf("\nPhilosopher %d has finished EATING", ph);
-        sem_post( & chopstick[left]);
+        sem_post(&chopsticks[left]);
         printf("\nPhilosopher %d puts down the left chopstick -> %d", ph, left);
-        sem_post( & chopstick[right]);
+        sem_post(&chopsticks[right]);
         printf("\nPhilosopher %d puts down the right chopstick -> %d ", ph, right);
     }
 }
