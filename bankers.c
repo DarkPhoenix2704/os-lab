281625a63f51b8b8e88515340762a812a703fa3d
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-10-06 23:21:04 +0530

    fix: bankers & fcfs

--- bankers.c
+++ bankers.c
@@ -1,79 +1,89 @@
 #include <stdio.h>
 
 int main() {
-    int Max[10][10], need[10][10], alloc[10][10], avail[10], completed[10], safeSequence[10];
-    int p, r, i, j, process, count;
-    count = 0;
-    printf("Enter the no of processes : ");
-    scanf("%d", & p);
-    for (i = 0; i < p; i++)
+    int max[10][10], need[10][10], alloc[10][10],avai[10],completed[10], safeSequence[10];
+    int p,r, count = 0, process;
+    printf("Enter the number of Process: ");
+    scanf("%d", &p); 
+    for(int i = 0; i < p; i++ )
         completed[i] = 0;
-    printf("\nEnter the no of resources : ");
-    scanf("%d", & r);
-    printf("Enter the Max Matrix for each process :\n");
-    for (i = 0; i < p; i++) {
-        printf("For process %d : ", i + 1);
-        for (j = 0; j < r; j++)
-            scanf("%d", & Max[i][j]);
+    printf("Enter the number of Resources: ");
+    scanf("%d", &r);
+    printf("Enter the max Matrix of each Process:\n");
+    for (int i = 0; i < p; i++)
+    {
+        printf("For Process %d : ", i+1);
+        for (int j = 0; j < r; j++)
+            scanf("%d", &max[i][j]);     
     }
-    printf("\n\nEnter the allocation for each process : ");
-    for (i = 0; i < p; i++) {
-        printf("\nFor process %d : ", i + 1);
-        for (j = 0; j < r; j++)
-            scanf("%d", & alloc[i][j]);
+    printf("Enter the Allocation Matrix of each Process:\n");
+    for (int i = 0; i < p; i++)
+    {
+        printf("For Process %d : ", i+1);
+        for (int j = 0; j < r; j++)
+            scanf("%d", &alloc[i][j]);     
     }
-    printf("\n\nEnter the Available Resources : ");
-    for (i = 0; i < r; i++)
-        scanf("%d", & avail[i]);
-    for (i = 0; i < p; i++)
-        for (j = 0; j < r; j++)
-            need[i][j] = Max[i][j] - alloc[i][j];
-    do {
-        printf("\nNeed matrix:\tAllocation matrix:\n");
-        for (i = 0; i < p; i++) {
-            for (j = 0; j < r; j++)
+    printf("Enter available resources: ");
+    for (int i = 0; i < r; i++)
+        scanf("%d", &avai[i]);
+    for (int i = 0; i < p; i++)
+    {
+        for (int j = 0; j < r; j++)
+            need[i][j] = max[i][j] - alloc[i][j];
+    }
+    do
+    {
+        printf("Need Matrix:\tAllocation Matrix:\n");
+        for (int i = 0; i < p; i++)
+        {
+            for (int j = 0; j < r; j++)
                 printf("%d ", need[i][j]);
-            printf("\t\t");
-            for (j = 0; j < r; j++)
-                printf("%d ", alloc[i][j]);
+            printf("\t\t\t");
+            for(int j = 0; j < r; j++)
+                printf("%d", alloc[i][j]);
             printf("\n");
         }
         process = -1;
-        for (i = 0; i < p; i++) {
-            if (completed[i] == 0) {
-                process = i;
-                for (j = 0; j < r; j++) {
-                    if (avail[j] < need[i][j]) {
-                        process = -1;
-                        break;
-                    }
-                }
+        for(int i = 0; i < p; i++) {
+            if (completed[i] == 0)
+            {
+               process = i;
+               for (int j = 0; j < r; j++) {
+                if (avai[j] < need[i][j]) 
+                {
+                    process = -1;
+                    break;
+                }   
+               }
             }
             if (process != -1)
                 break;
         }
-        if (process != -1) {
-            printf("\nProcess %d runs to completion!", process + 1);
-            safeSequence[count] = process + 1;
+        if (process != -1)
+        {
+            printf("\nProcess %d runs to Completion!", process+1);
+            safeSequence[count] = process+1;
             count++;
-            for (j = 0; j < r; j++) {
-                avail[j] += alloc[process][j];
+            for(int j = 0; j < r; j++) {
+                avai[j] = avai[j] + alloc[process][j];
                 alloc[process][j] = 0;
                 need[process][j] = 0;
                 completed[process] = 1;
             }
-            printf("\nNow Avaialble: ");
-            for (i = 0; i < r; i++)
-                printf("%d ", avail[i]);
+            printf("\nNow Available: ");
+            for (int i = 0; i < r; i++)
+                printf("%d", avai[i]);
             printf("\n");
         }
-    } while (count != p && process != -1);
-    if (count == p) {
-        printf("\nThe system is in a safe state!!\n");
-        printf("Safe Sequence : < ");
-        for (i = 0; i < p; i++)
-            printf("P%d ", safeSequence[i]);
-        printf(">\n");
-    } else
-        printf("\nThe system is in an unsafe state!!");
+    } while (count != p && process != -1 );
+    if (count == p)
+    {
+        printf("\nThe System is in Safe State!!\n");
+        printf("Safe Sequence is :<");
+        for(int i = 0; i < p; i++){
+            printf("%d,", safeSequence[i]);
+        }
+    } else {
+        printf("\nThe System is in Unsafe State");
+    }   
 }
--- fcfs.c
+++ fcfs.c
@@ -1,76 +1,77 @@
 #include<stdio.h>
-#include<strings.h>
+#include<string.h>
 
 void getData();
+void fcfs();
 void calculate();
 void display();
-void fcfs();
-int bt[20], at[20], wt[20], et[20], twt = 0, temp1, temp2, ttt = 0, tt[20], n;
-char pName[20][20], temp[20];
-float awt, att;
+
+int arrivalTime[10], burstTime[10], waitingTime[10], exitTime[10], turnAroundTime[10];
+int n, temp, totalWaitingTime, totalTurnAroundTime;
+float averageWaitingTime, averageTurnAroundTime;
+char pName[20][20], temp1[20];
 void main() {
     getData();
     fcfs();
     calculate();
     display();
 }
+
 void getData() {
-    printf("Enter the No of Process : ");
-    scanf("%d", & n);
-    for (int i = 1; i <= n; i++) {
-        printf("\nEnter the Process Name: ");
-        scanf("%s", & pName[i]);
+    printf("\nEnter the number of Process: ");
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        printf("\nEnter the Procees Name: ");
+        scanf("%s", &pName[i]);
         printf("Enter the Arrival Time: ");
-        scanf("%d", & at[i]);
+        scanf("%d", &arrivalTime[i]);
         printf("Enter the Burst Time: ");
-        scanf("%d", & bt[i]);
+        scanf("%d", &burstTime[i]);
     }
 }
+
 void fcfs() {
-    for (int i = 1; i <= n; i++) {
-        for (int j = i + 1; j <= n; j++) {
-            if (at[i] > at[j]) {
-                temp1 = at[i];
-                at[i] = at[j];
-                at[j] = temp1;
-                temp2 = bt[i];
-                bt[i] = bt[j];
-                bt[j] = temp2;
-                strcpy(temp, pName[i]);
-                strcpy(pName[i], pName[j]);
-                strcpy(pName[j], temp);
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j <= n; j++) {
+            if (arrivalTime[i] > arrivalTime[j]) {
+                temp = arrivalTime[j];
+                arrivalTime[j] = arrivalTime[i];
+                arrivalTime[i] = temp;
+                temp = burstTime[j];
+                burstTime[j] = burstTime[i];
+                burstTime[i] = temp;
+                strcpy(temp1, pName[j]);
+                strcpy(pName[j], pName[i]);
+                strcpy(pName[i], temp1);
             }
         }
     }
 }
 void calculate() {
-    wt[1] = 0;
-    tt[1] = 0;
-    tt[1] = bt[1];
-    et[1] = bt[1];
-    et[1] = bt[1] + at[1];
-    ttt = tt[1];
-    twt = wt[1];
-    for (int i = 2; i <= n; i++) {
-        wt[i] = et[i - 1] - at[i];
-        tt[i] = wt[i] + bt[i];
-        et[i] = et[i - 1] + bt[i];
-        twt = twt + wt[i];
-        ttt = ttt = tt[i];
+    waitingTime[0] = 0;
+    turnAroundTime[0] = burstTime[0] + waitingTime[0];
+    exitTime[0] = burstTime[0] + arrivalTime[0];
+    totalTurnAroundTime = turnAroundTime[0];
+    totalWaitingTime = waitingTime[0];
+    for(int i = 1; i <= n; i++) {
+        waitingTime[i] = exitTime[i-1] - arrivalTime[i];
+        turnAroundTime[i] = burstTime[i] + waitingTime[i];
+        exitTime[i] = exitTime[i-1] +  burstTime[i];
+        totalTurnAroundTime = totalTurnAroundTime + turnAroundTime[i];
+        totalWaitingTime = totalWaitingTime + waitingTime[i];
     }
-    awt = (float) twt / n;
-    att = (float) ttt / n;
+    averageTurnAroundTime = (float) totalTurnAroundTime / n;
+    averageWaitingTime = (float) totalWaitingTime / n;
 }
+
 void display() {
-    printf("\n\n\t\tGantt Chart\n");
-    printf("\n____________________________________\n");
-    for (int i = 1; i <= n; i++) {
+    printf("\n\nGantt Chart\n");
+    printf("\n__________________________________\n");
+    for(int i = 0; i <= n; i++)
         printf("\t%s\t", pName[i]);
-    }
-    printf("\n____________________________________\n");
-    for (int i = 1; i <= n; i++) {
-        printf("\t\t%d\t", et[i]);
-    }
-    printf("\n\nAvg=%f", awt);
-    printf("\nWt=%f", att);
+    printf("\n__________________________________\n");
+    for(int i = 0; i <= n; i++)
+        printf("\t%d\t", exitTime[i]);
+    printf("\nAvarage TAT=%f", averageTurnAroundTime);
+    printf("\nAverage WT =%f", averageWaitingTime);
 }
